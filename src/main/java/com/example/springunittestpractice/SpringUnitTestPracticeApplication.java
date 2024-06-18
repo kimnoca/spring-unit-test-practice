8e2f9cff3fa910901559fca25d8b6e648b8a99ce
kimnoca <kimnoca@naver.com>
2024-06-18 17:12:48 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/com/example/springunittestpractice/SpringUnitTestPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springunittestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUnitTestPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUnitTestPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springunittestpractice/SpringUnitTestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springunittestpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUnitTestPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
