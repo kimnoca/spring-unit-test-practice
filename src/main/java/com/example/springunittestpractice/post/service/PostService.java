2c0e88068c7b5235bb83a2ec8eae831ddef4ba0e
kimnoca <kimnoca@naver.com>
2024-06-19 13:38:57 +0900

    feat : postId로 게시물 찾기 관련 service layer 구현

--- src/main/java/com/example/springunittestpractice/post/service/PostService.java
+++ src/main/java/com/example/springunittestpractice/post/service/PostService.java
@@ -3,7 +3,6 @@ package com.example.springunittestpractice.post.service;
 import com.example.springunittestpractice.post.domain.Post;
 import com.example.springunittestpractice.post.dto.PostCreateDto;
 import com.example.springunittestpractice.post.repository.PostRepository;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,15 +15,15 @@ public class PostService {
     private final PostRepository postRepository;
 
     public Post savePost(PostCreateDto postCreateDto) {
-        Post post = Post.builder()
+
+        return Post.builder()
                 .title(postCreateDto.getTitle())
                 .content(postCreateDto.getContent())
                 .build();
-
-        return post;
     }
 
-    public List<Post> findAll() {
-        return postRepository.findAll();
+    public Post findById(Long id) {
+        return postRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다."));
     }
+
 }
